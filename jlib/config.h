7ccc749e0f32e06aa498c3ab3e330eef78983cdc
465084127@qq.com <465084127@qq.com>
2019-12-26 04:51:17 -0500

    jj

--- jlib/config.h
+++ jlib/config.h
@@ -6,11 +6,12 @@
 #define JW_ENABLE_LOG 1
 
 class noncopyable
-{  
-protected:  
-    noncopyable() {}  
-    ~noncopyable() {}  
+{
+protected:
+	noncopyable() {}
+	~noncopyable() {}
+
 private:
-    noncopyable( const noncopyable& ) = delete;  
-    const noncopyable& operator=(const noncopyable&) = delete;  
+	noncopyable(const noncopyable &) = delete;
+	const noncopyable &operator=(const noncopyable &) = delete;
 };
